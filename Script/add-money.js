2c1fbaa5484bea1ba263cc23b5bb17059322282e
ibrahim3761 <mdibrahimabdullah1@gmail.com>
2025-02-25 00:07:55 +0600

    add money added

--- Script/add-money.js
+++ Script/add-money.js
@@ -1,14 +1,24 @@
-document.getElementById('add-money').addEventListener('click',
-    function(event) {
-        event.preventDefault();
-        const accAmount = document.getElementById('acc-amount').value;
-        const conAmount = parseFloat(accAmount);
-        const pin = document.getElementById('pin-num').value;
-        const mainBal = document.getElementById('main-bal').innerText;
-        const conBal = parseFloat(mainBal);
-        if (pin === '1234') {
-            const sum = conBal + conAmount;
-            document.getElementById('main-bal').innerText = sum;
-            document.getElementById('acc-amount').value = '';
-        }
-})
+document
+  .getElementById("add-money")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const accAmount = document.getElementById("acc-amount").value;
+    const conAmount = parseFloat(accAmount);
+    const pin = document.getElementById("pin-num").value;
+    const conPin = parseInt(pin);
+    const mainBal = document.getElementById("main-bal").innerText;
+    const conBal = parseFloat(mainBal);
+    if (conPin && accAmount) {
+      if (conPin === 1234) {
+        const sum = conBal + conAmount;
+        document.getElementById("main-bal").innerText = sum;
+        document.getElementById("acc-amount").value = "";
+      }
+      else {
+        alert("Invalid Pin");
+      }
+    } 
+    else {
+      alert("Enter Amount and Pin");
+    }
+  });
