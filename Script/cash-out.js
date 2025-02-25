55112f8b77ae9886477e202a0a7054f3211561cf
ibrahim3761 <mdibrahimabdullah1@gmail.com>
2025-02-26 01:04:46 +0600

    cashOut

--- Script/cash-out.js
+++ Script/cash-out.js
@@ -1,21 +1,19 @@
 document
-  .getElementById("add-money")
+  .getElementById("cash-out-btn")
   .addEventListener("click", function (event) {
     event.preventDefault();
-    const accAmount = document.getElementById("acc-amount").value;
-    const conAmount = parseFloat(accAmount);
-    const pin = document.getElementById("pin-num").value;
+    const pin = document.getElementById("cashout-pin-num").value;
     const conPin = parseInt(pin);
+    const accAmount = document.getElementById("cashout-amount").value;
+    const conAmount = parseFloat(accAmount);
     const mainBal = document.getElementById("main-bal").innerText;
     const conBal = parseFloat(mainBal);
-    
     if (conPin && accAmount) {
       if (conPin === 1234) {
-        const sum = conBal + conAmount;
-        document.getElementById("main-bal").innerText = sum;
+        const Bal = conBal - conAmount;
+        document.getElementById("main-bal").innerText = Bal;
         document.getElementById("acc-amount").value = "";
-      }
-      else {
+      } else {
         alert("Invalid Pin");
       }
     } 
